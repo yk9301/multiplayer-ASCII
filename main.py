e37acbe85017f73937e02dc58b107b536aef75a2
yk9301 <yannis.kubach@web.de>
2024-09-23 13:15:08 +0100

    new coord

--- main.py
+++ main.py
@@ -34,7 +34,6 @@ def on_press(key):
     except AttributeError:
         print('special key {0} pressed'.format(key))
 
-
 def on_release(key):
     # print('{0} released'.format(key))
     if key == keyboard.Key.esc:
