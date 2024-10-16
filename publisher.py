7a3cad15ce78a2de653467bcb46849fbd4099cb6
yk9301 <yannis.kubach@web.de>
2024-10-16 14:56:09 +0200

    ip change

--- publisher.py
+++ publisher.py
@@ -3,7 +3,7 @@ import paho.mqtt.client as mqtt
 
 def publisher(message: str, player, topic= "update"):
     
-    broker_address = "192.168.86.72" 
+    broker_address = "10.126.65.77"  
     client = mqtt.Client(client_id="Publisher" + str(player), protocol=mqtt.MQTTv311)
     client.connect(broker_address, 1883, 60)
     client.publish(topic, message)
